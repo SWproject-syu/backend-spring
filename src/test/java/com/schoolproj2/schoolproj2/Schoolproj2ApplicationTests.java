78a429d537c3627a6ef22618f01c4ae733c507c9
Mins97 <tkarnrwl7862@naver.com>
2022-04-24 18:28:12 +0900

    backup minsekim

---
+++ src/test/java/com/schoolproj2/schoolproj2/Schoolproj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolproj2.schoolproj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Schoolproj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

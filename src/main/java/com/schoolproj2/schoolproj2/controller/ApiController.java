5025383cdf73eec2f0d7a658cee677ec43ebb6c5
Mins97 <tkarnrwl7862@naver.com>
2022-04-29 01:21:45 +0900

    은행검색 완료:

--- src/main/java/com/schoolproj2/schoolproj2/controller/ApiController.java
+++ src/main/java/com/schoolproj2/schoolproj2/controller/ApiController.java
@@ -1,6 +1,8 @@
 package com.schoolproj2.schoolproj2.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,36 +14,34 @@ import java.net.URL;
 
 @RequiredArgsConstructor
 @RestController
-
+@CrossOrigin("*")
 public class ApiController {
-    @GetMapping("/api")
+    @GetMapping("/search")
     public String callApi() throws IOException {
         StringBuilder result = new StringBuilder();
         StringBuffer reult = new StringBuffer();
-        String urlStr =" http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth=e9bbf74e2601259689d7ffe71a8a9707&topFinGrpNo=020000&pageNo=1";
+        String urlStr = "http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth=e9bbf74e2601259689d7ffe71a8a9707&topFinGrpNo=020000&pageNo=1";
 
         URL url = new URL(urlStr);
+
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         urlConnection.setRequestMethod("GET");
+
         BufferedReader br;
-        br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
+        br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
 
         String returnLine;
 
-        while((returnLine = br.readLine()) != null){
-            result.append(returnLine+"\n\r");
+        while ((returnLine = br.readLine()) != null) {
+            result.append(returnLine + "\n\r");
 
         }
         urlConnection.disconnect();
 
         return result.toString();
 
-
     }
-
-
-
 
 }
 
-//  e9bbf74e2601259689d7ffe71a8a9707
+// e9bbf74e2601259689d7ffe71a8a9707

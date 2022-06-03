24fc1210d4cb5f57982c1d6850351b67d07f645c
minsekim1 <tkarnrwl7862@naver.com>
2022-06-03 23:49:05 +0900

    merge

--- src/main/java/com/schoolproj2/schoolproj2/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.schoolproj2.schoolproj2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-    Optional<Member> findByEmail(String userEmail);
-
-}
